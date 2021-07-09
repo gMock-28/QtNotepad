b187a52c9175b927092159a3056a3a654cb1c3c8
Dmitry <dmitry.bondarenko.2805@gmail.com>
2021-07-09 13:24:08 +0300

    Made: Notepad project, realization of the Notepad, open and save funcs.

---
+++ QtNotepad/mainwindow.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <QMainWindow>
+#include <QtWidgets>
+
+namespace Ui { class MainWindow; }
+
+class MainWindow : public QMainWindow {
+ Q_OBJECT
+
+public:
+     explicit MainWindow(QApplication *parent = 0);
+     ~MainWindow();
+private slots:
+     void open(); //метод для открытия файла
+     void save(); //метод для сохранения файла
+private:
+     Ui::MainWindow *ui;
+     QTextEdit *textEdit; //указатель на поле ввода текста
+     QAction *openAction; //указатели на действия "Открыть",
+     QAction *saveAction; //"Сохранить"
+     QAction *exitAction; //и "Выйти"
+     QMenu *fileMenu; //Указатель на меню
+};
